32bc9f8987f7c560ed9846752c28ebaed791b4a6
tdenise <t.denise44@yahoo.com>
2019-11-18 23:16:31 -0800

    test 8

--- web/upload2.php
+++ web/upload2.php
@@ -1,44 +1,54 @@
-<!--script that accepts a file to upload in the browser, and stores it on S3 under the same name it had on the client’s computer.-->
 <?php
-require('vendor/autoload.php');
 
-// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
-$s3 = new Aws\S3\S3Client([
-    'version'  => 'latest',
-    'region'   => 'us-west-1',
-]);
-$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
-?>
-
-
-
-<!--_____________________________________________________-->
-<?php
-session_start();
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
-if (isset($_POST['submitD'])) {	
-	$filesName = $_FILES['file']['name'];
-	$filesTmpName = $_FILES['file']['tmp_name'];
-	$filesSize = $_FILES['file']['size'];
-	$filesType = $_FILES['file']['type'];
-
-	$fileExt = explode('.', $filesName);
-	$fileActualExt = strtolower(end($fileExt));
+	require './vendor/autoload.php';
 	
-	$allowedExt = array('jpg', 'jpeg', 'png');
-	
-	if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['myfile']) && $_FILES['myfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['myfile']['tmp_name'])){
-		if($filesError === 0){
-            $upload = $s3->upload($bucket, $_FILES['myfile']['name'], fopen($_FILES['myfile']['tmp_name'], 'rb'), 'public-read');
-			header("Location: ../../index.html?uploadsuccess");
-		} else {
-			echo "Error Uploading File";
-			header("Location: ../../upload2.html?uploadFail");
-		}
-	} else {
-		echo "Invalid File Type\n";
-		echo "Valid File Types: jpg, jpeg, png";
-		header("Location: ../../upload2.html?uploadFail");
+	use Aws\S3\S3Client;
+	use Aws\S3\Exception\S3Exception;
+	// AWS Info
+	$bucketName = 'realdress';
+	$IAM_KEY = 'AKIA5FXIKMXZYLR7J3QX';
+	$IAM_SECRET = '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';
+	// Connect to AWS
+	try {
+		// You may need to change the region. It will say in the URL when the bucket is open
+		// and on creation.
+		$s3 = S3Client::factory(
+			array(
+				'credentials' => array(
+					'key' => $IAM_KEY,
+					'secret' => $IAM_SECRET
+				),
+				'version' => 'latest',
+				'region'  => 'us-west-1'
+			)
+		);
+	} catch (Exception $e) {
+		// We use a die, so if this fails. It stops here. Typically this is a REST call so this would
+		// return a json object.
+		die("Error: " . $e->getMessage());
 	}
-}}
+	
+	// For this, I would generate a unqiue random string for the key name. But you can do whatever.
+	$keyName = 'test_example/' . basename($_FILES["fileToUpload"]['name']);
+	$pathInS3 = 'https://s3.us-west-1.amazonaws.com/' . $bucketName . '/' . $keyName;
+	// Add it to S3
+	try {
+		// Uploaded:
+		$file = $_FILES["fileToUpload"]['tmp_name'];
+		$s3->putObject(
+			array(
+				'Bucket'=>$bucketName,
+				'Key' =>  $keyName,
+				'SourceFile' => $file,
+				'StorageClass' => 'REDUCED_REDUNDANCY'
+			)
+		);
+	} catch (S3Exception $e) {
+		die('Error:' . $e->getMessage());
+	} catch (Exception $e) {
+		die('Error:' . $e->getMessage());
+	}
+	echo 'Done';
+	// Now that you have it working, I recommend adding some checks on the files.
+	// Example: Max size, allowed file types, etc.
 ?>
