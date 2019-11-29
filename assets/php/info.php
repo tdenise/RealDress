9429eae8d31f351e53ea5c7aa01eed4b170bec4d
Noah Miles <noahemiles@gmail.com>
2019-11-29 10:38:56 -0800

    added waist field

--- assets/php/info.php
+++ assets/php/info.php
@@ -1,20 +1,21 @@
 <?php
 session_start();
-if (isset($_POST['height']) && isset($_POST['weight']) && isset($_POST['chest']) && isset($_POST['hips'])) {
+
+if (isset($_POST['height']) && isset($_POST['chest']) && isset($_POST['waist']) && isset($_POST['hips'])) {
 	$height = $_POST['height'];
-	$weight = $_POST['weight'];
+	$waist = $_POST['waist'];
 	$chest = $_POST['chest'];
 	$hips = $_POST['hips'];
 	$targetFolder = "../profile/";
 	$fileName = $targetFolder."User_".uniqid('', true).".txt";
 	/* File Output Format:
 	height value
-	weight value
+	waist value
 	chest value
 	hips value
 	*/
 	file_put_contents($fileName, $height."\n");
-	file_put_contents($fileName, $weight."\n", FILE_APPEND);
+	file_put_contents($fileName, $waist."\n", FILE_APPEND);
 	file_put_contents($fileName, $chest."\n", FILE_APPEND);
 	file_put_contents($fileName, $hips."\n", FILE_APPEND);
 	header("Location: ../../index.html?userfilecreated");
