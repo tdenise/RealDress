67e54618de84de5500eab49867d354aac7a194ff
Noah Miles <noahemiles@gmail.com>
2019-12-02 18:30:01 -0800

    moved _dress to before file name

--- web/upload2.php
+++ web/upload2.php
@@ -32,7 +32,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
 	}
 	
 	// For this, I would generate a unqiue random string for the key name. But you can do whatever.
-	$keyName = 'user_uploads/' . basename($_FILES['myFile']['name'] . "_dress");
+	$keyName = 'user_uploads/' . "dress_" . basename($_FILES['myFile']['name']);
 	$pathInS3 = 'https://s3.us-west-1.amazonaws.com/' . $bucketName . '/' . $keyName;
 	// Add it to S3
 	$fileExt = explode('.', $_FILES['myFile']['name']);
