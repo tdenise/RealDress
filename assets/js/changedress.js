af31c12f1160c9c9b76e629d260b9a6309fbe778
Noah Miles <noahemiles@gmail.com>
2019-12-09 15:06:07 -0800

    moved linked list and change dress to file changedress.js

---
+++ assets/js/changedress.js
@@ -0,0 +1,45 @@
+	  class Node {
+		constructor(element)
+		{
+			this.element = element;
+			this.next = null;
+		}
+	  }
+	  class LinkedList{
+		constructor()
+		{
+			this.head = null;
+			this.size = 0;
+		}
+		add(element)
+		{
+			var node = new Node(element);
+			var current;
+			if (this.head == null) 
+				this.head = node; 
+			else { 
+				current = this.head;  
+				while (current.next) { 
+					current = current.next; 
+				} 
+				current.next = node; 
+			} 
+			this.size++;
+		} 
+	  }
+		var dressList = new LinkedList();
+		dressList.add("/assets/img/dress1.png");
+		dressList.add("/assets/img/dress2.png");
+		dressList.add("/assets/img/dress3.png");
+
+		var curr = dressList.head;
+		function changeDress()
+		{
+			document.getElementById("clothes").style.backgroundImage = "url(" + curr.element + ")";
+				if(curr.next != null){
+				curr = curr.next;
+				} else {
+				curr = dressList.head;
+				}	
+		}
+		
