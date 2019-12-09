cda1fb899244940ca10324730519365bb16a472f
Denise Tran <tdenise@users.noreply.github.com>
2019-12-09 04:57:38 -0800

    test

--- web/get.php
+++ web/get.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     $bucketName = 'realdress';
     $IAM_KEY = 'AKIA5FXIKMXZYLR7J3QX';
@@ -13,7 +12,7 @@
   $accessCode = addslashes($accessCode);
   $accessCode = htmlspecialchars($accessCode);
   // Connect to database
-  $con = mysqli_connect('localhost', 'root', 'root', 's3DB') or die('Error: Unable to connect');
+  $con = mysqli_connect('localhost', 'root', 'root', 's3db') or die('Error: Unable to connect');
   // Verify valid access code
   $result = mysqli_query($con, "SELECT * FROM s3Files WHERE accessCode='$accessCode'") or die("Error: Invalid request");
   if (mysqli_num_rows($result) != 1) {
@@ -48,55 +47,4 @@
     echo $result['Body'];
   } catch (Exception $e) {
     die("Error: " . $e->getMessage());
-=======
-<?php
-    $bucketName = 'realdress';
-    $IAM_KEY = 'AKIA5FXIKMXZYLR7J3QX';
-	$IAM_SECRET = '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';
-  require '../vendor/autoload.php';
-  use Aws\S3\S3Client;
-  use Aws\S3\Exception\S3Exception;
-  // Get the access code
-  $accessCode = $_GET['c'];
-  $accessCode = strtoupper($accessCode);
-  $accessCode = trim($accessCode);
-  $accessCode = addslashes($accessCode);
-  $accessCode = htmlspecialchars($accessCode);
-  // Connect to database
-  $con = mysqli_connect('localhost', 'root', 'root', 's3DB') or die('Error: Unable to connect');
-  // Verify valid access code
-  $result = mysqli_query($con, "SELECT * FROM s3Files WHERE accessCode='$accessCode'") or die("Error: Invalid request");
-  if (mysqli_num_rows($result) != 1) {
-    die("Error: Invalid access code");
-  }
-  
-  // Get path from db
-  $keyPath = '';
-  while($row = mysqli_fetch_array($result)) {
-    $keyPath = $row['s3FilePath'];
-  }
-  // Get file
-  try {
-    $s3 = S3Client::factory(
-      array(
-        'credentials' => array(
-          'key' => $IAM_KEY,
-          'secret' => $IAM_SECRET
-        ),
-        'version' => 'latest',
-        'region'  => 'us-west-1'
-      )
-    );
-    //
-    $result = $s3->getObject(array(
-      'Bucket' => $BUCKET_NAME,
-      'Key'    => $keyPath
-    ));
-    // Display it in the browser
-    header("Content-Type: {$result['ContentType']}");
-    header('Content-Disposition: filename="' . basename($keyPath) . '"');
-    echo $result['Body'];
-  } catch (Exception $e) {
-    die("Error: " . $e->getMessage());
->>>>>>> 2694fa9a2b4695fcfa1bad213e3f5c7ee60933ee
   }
