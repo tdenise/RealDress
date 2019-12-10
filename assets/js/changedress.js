3aefc507e157dbffb3dbe651a3b98110a5e8f486
Noah Miles <noahemiles@gmail.com>
2019-12-10 11:33:00 -0800

    trying anything

--- assets/js/changedress.js
+++ assets/js/changedress.js
@@ -28,26 +28,5 @@
 		} 
 	  }
 	  
-		var dressList = new LinkedList();
-		console.log("dresslist created");
-		var current = dressList.head;  
-				while (current.next) { 
-					console.log(current.element);
-					current = current.next; 
-				} 
-	
-		//dressList.add("https://realdress.s3-us-west-1.amazonaws.com/user_uploads/dress1S.png");
-		//dressList.add("https://realdress.s3-us-west-1.amazonaws.com/user_uploads/dress2S.png");
-		//dressList.add("https://realdress.s3-us-west-1.amazonaws.com/user_uploads/dress3.png");
 
-		var curr = dressList.head;
-		function changeDress()
-		{
-			document.getElementById("clothes").style.backgroundImage = "url(" + curr.element + ")";
-				if(curr.next != null){
-				curr = curr.next;
-				} else {
-				curr = dressList.head;
-				}	
-		}
 		
