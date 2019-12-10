30d371e1a8a03998876a22307d2e32e37aba6310
Denise Tran <tdenise@users.noreply.github.com>
2019-12-09 16:20:15 -0800

    Add database info file

---
+++ database.php
@@ -0,0 +1,26 @@
+<?php
+    $active_group = 'default';
+    $query_builder = TRUE;
+
+    $db['default'] = array(
+        'dsn' => '',
+        'hostname' => 'us-cdbr-iron-east-05.cleardb.net',
+        'username' => 'bb72072205ffd6',
+        'password' => '02e9938c',
+        'database' => 'heroku_8f1b3bade09a482',
+        'dbdriver' => 'mysqli',
+        'dbprefix' => '',
+        'pconnect' => FALSE,
+        'db_debug' => (ENVIRONMENT !== 'production'),
+        'cache_on' => FALSE,
+        'cachedir' => '',
+        'char_set' => 'utf8',
+        'dbcollat' => 'utf8_general_ci',
+        'swap_pre' => '',
+        'encrpt' => FALSE,
+        'compress' => FALSE,
+        'striction' => FALSE,
+        'failover' => array(),
+        'save_queries' => 'TRUE'
+    )
+?>
