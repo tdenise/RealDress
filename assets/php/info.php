b19294e4a5c84e76619550ecf23e9cc1103275a2
Noah Miles <noahemiles@gmail.com>
2019-12-09 19:16:20 -0800

    debug fun

--- assets/php/info.php
+++ assets/php/info.php
@@ -65,6 +65,6 @@ if (isset($_POST['height']) && isset($_POST['chest']) && isset($_POST['waist']) 
 	//file_put_contents($fileName, $waist."\n", FILE_APPEND);
 	//file_put_contents($fileName, $hips."\n", FILE_APPEND);
 	//file_put_contents($fileName, $size_calculated."\n", FILE_APPEND);
-	header("Location: ../../index.html?userfilecreated");
+	header("Location: ../../web/get.php");
 }
 ?>
