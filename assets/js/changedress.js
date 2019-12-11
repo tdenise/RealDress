4b1246b89afd2c8a3a15b8de021fb44cb404fc83
Noah Miles <noahemiles@gmail.com>
2019-12-11 14:27:42 -0800

    debug

--- assets/js/changedress.js
+++ assets/js/changedress.js
@@ -26,16 +26,30 @@ class LinkedList {
 }
 var dressList = new LinkedList();
 var passedArray;
-$.get("../../web/get.php", function(data){
-	passedArray = data 
-	
+$(document).ready( function() {
+    $('#prev').click(function() {
+        $.ajax({
+            type: 'POST',
+            url: '../../web/get.php',
+            data: 'id=testdata',
+            dataType: 'json',
+            cache: false,
+            success: function(result) {
+                var arrayLength = result.length;
+				for (var i = 0; i < arrayLength; i++) {
+					dressList.add(result[i]);
+					console.log("Dress: "+result[i]);
+				}
+            },
+        });
+    });
 });
 //var passedArray = <?php echo json_encode($dressArray); ?>;
-var arrayLength = passedArray.length;
-for (var i = 0; i < arrayLength; i++) {
-	dressList.add(passedArray[i]);
-	console.log("Dress: "+passedArray[i]);
-}
+//var arrayLength = passedArray.length;
+//for (var i = 0; i < arrayLength; i++) {
+//	dressList.add(passedArray[i]);
+//	console.log("Dress: "+passedArray[i]);
+//}
 //dressList.add("https://realdress.s3-us-west-1.amazonaws.com/user_uploads/dress1S.png");
 //dressList.add("https://realdress.s3-us-west-1.amazonaws.com/user_uploads/dress2S.png");
 //dressList.add("https://realdress.s3-us-west-1.amazonaws.com/user_uploads/dress3.png");
